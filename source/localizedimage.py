b1ab5274954f0e4bfc5fef1c532d28234ab176dc
Jeremy Morel <jeremy.morel@opendatasoft.com>
2017-04-06 18:10:01 +0200

    new directive: localizedimage

---
+++ source/localizedimage.py
@@ -0,0 +1,33 @@
+import os
+from docutils.parsers.rst.directives.images import Image
+
+
+def _localized_file_path(file_path, language):
+    index = file_path.rfind('.')
+    return file_path[:index] + '--' + language + file_path[index:]
+
+
+def _localized_file_exists(env, file_path):
+    path = env.relfn2path(file_path, env.docname)[1]
+    return os.path.exists(path)
+
+
+class Localizedimage(Image):
+    def run(self):
+        # generate correct file path
+        base_file_path = self.arguments[0]
+        env = self.state.document.settings.env
+
+        # try in specified language
+        file_path = _localized_file_path(base_file_path, env.config.language)
+
+        # fallback to english
+        if not _localized_file_exists(env, file_path):
+            file_path = _localized_file_path(base_file_path, 'en')
+
+        self.arguments[0] = file_path
+        return super(Localizedimage, self).run()
+
+
+def setup(app):
+    app.add_directive('localizedimage', Localizedimage)
